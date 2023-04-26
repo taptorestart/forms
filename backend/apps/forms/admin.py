b1ad49ba01d5e6abf4ff05c077daff7888f5bfdc
taptorestart <taptorestart@gmail.com>
2023-04-26 17:00:41 +0900

    ENH: Improve modelling

--- backend/apps/forms/admin.py
+++ backend/apps/forms/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.forms.models import Form, Component, Choice
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "slug",
+        "title",
+        "start_date",
+        "end_date",
+        "updated_by",
+        "created_at",
+        "update_at",
+    )
+    readonly_fields = ("updated_by",)
+
+    def save_model(self, request, obj: Form, form, change):
+        obj.updated_by_id = request.user.id
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "form_slug",
+        "form_title",
+        "type",
+        "is_question",
+        "max_allowed_size",
+        "title",
+        "description",
+        "order",
+        "updated_by",
+        "created_at",
+        "update_at",
+    )
+    readonly_fields = (
+        "updated_by",
+        "is_question",
+    )
+    raw_id_fields = ("form",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related("form", "updated_by")
+        return queryset
+
+    def form_slug(self, obj: Component) -> str:
+        return obj.form.slug
+
+    def form_title(self, obj: Component) -> str:
+        return obj.form.title
+
+    def save_model(self, request, obj: Component, form, change):
+        obj.updated_by_id = request.user.id
+        if not change:
+            order_list = Component.objects.filter(form_id=obj.form_id).values_list("order", flat=True)
+            obj.order = max(order_list) + 1 if order_list else 1
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "component_title",
+        "text",
+        "order",
+        "updated_by",
+        "created_at",
+        "update_at",
+    )
+    readonly_fields = ("updated_by",)
+    raw_id_fields = ("component",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related("component", "updated_by")
+        return queryset
+
+    def component_title(self, obj: Choice) -> str:
+        return obj.component.title
+
+    def save_model(self, request, obj: Choice, form, change):
+        obj.updated_by_id = request.user.id
+        if not change:
+            order_list = Choice.objects.filter(component_id=obj.component_id).values_list("order", flat=True)
+            obj.order = max(order_list) + 1 if order_list else 1
+        super().save_model(request, obj, form, change)
--- backend/apps/forms/models.py
+++ backend/apps/forms/models.py
@@ -1,45 +1,136 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Form(models.Model):
-    slug = models.SlugField(max_length=100, db_comment="slug")
-    title = models.CharField(max_length=255, db_comment="title")
-    start_date = models.DateTimeField(db_comment="start date")
-    end_date = models.DateTimeField(db_comment="end date")
-    created_at = models.DateTimeField(auto_now_add=True, null=True, db_comment="created at")
-    update_at = models.DateTimeField(auto_now=True, null=True, db_comment="updated at")
+    slug = models.SlugField(verbose_name=_("slug"), max_length=100, db_comment="slug")
+    title = models.CharField(verbose_name=_("title"), max_length=255, db_comment="title")
+    start_date = models.DateTimeField(verbose_name=_("start date"), db_comment="start date")
+    end_date = models.DateTimeField(verbose_name=_("end date"), db_comment="end date")
+    updated_by = models.ForeignKey(
+        User,
+        verbose_name=_("updated by"),
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        db_comment="last user who updated",
+    )
+    created_at = models.DateTimeField(verbose_name=_("created at"), auto_now_add=True, db_comment="created at")
+    update_at = models.DateTimeField(verbose_name=_("updated at"), auto_now=True, db_comment="updated at")
 
     class Meta:
         db_table = "form"
+        verbose_name = _("form")
+        verbose_name_plural = _("forms")
 
 
 class Component(models.Model):
-    form = models.ForeignKey(Form, on_delete=models.CASCADE, db_comment="form FK")
-    type = models.CharField(max_length=20, db_comment="type")
-    title = models.CharField(max_length=255, db_comment="title")
-    note = models.CharField(max_length=500, db_comment="note")
-    order = models.SmallIntegerField(db_comment="order")
+    TEXT = 101
+    INPUT, TEXTAREA, RADIO, CHECKBOX, SELECT = 201, 202, 203, 204, 205
+    TYPE_CHOICES = (
+        (TEXT, "TEXT"),
+        (INPUT, "INPUT"),
+        (TEXTAREA, "TEXTAREA"),
+        (RADIO, "RADIO"),
+        (CHECKBOX, "CHECKBOX"),
+        (SELECT, "SELECT"),
+    )
+    QUESTION_TYPES = (INPUT, TEXTAREA, RADIO, CHECKBOX, SELECT)
+
+    form = models.ForeignKey(Form, related_name="+", on_delete=models.CASCADE, db_comment="form FK")
+    type = models.SmallIntegerField(verbose_name=_("type"), choices=TYPE_CHOICES, db_comment="type")
+    max_allowed_size = models.SmallIntegerField(
+        verbose_name=_("max allowed size"), null=True, blank=True, db_comment="max allowed size"
+    )
+    title = models.CharField(verbose_name=_("title"), null=True, blank=True, max_length=255, db_comment="title")
+    description = models.CharField(
+        verbose_name=_("description"), null=True, blank=True, max_length=500, db_comment="description"
+    )
+    order = models.SmallIntegerField(verbose_name=_("order"), default=0, db_comment="order")
+    updated_by = models.ForeignKey(
+        User,
+        verbose_name=_("updated by"),
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        db_comment="last user who updated",
+    )
+    created_at = models.DateTimeField(verbose_name=_("created at"), auto_now_add=True, db_comment="created at")
+    update_at = models.DateTimeField(verbose_name=_("updated at"), auto_now=True, db_comment="updated at")
 
     class Meta:
         db_table = "component"
+        verbose_name = _("component")
+        verbose_name_plural = _("components")
 
-
-class Question(models.Model):
-    component = models.ForeignKey(Component, on_delete=models.CASCADE, db_comment="component FK")
-    title = models.CharField(max_length=255, db_comment="title")
-    note = models.CharField(max_length=500, db_comment="note")
-    type = models.CharField(max_length=20, db_comment="type")
-    allowed_size = models.SmallIntegerField(db_comment="allowed_size")
-    is_required = models.BooleanField(db_comment="is required")
-
-    class Meta:
-        db_table = "question"
+    @property
+    def is_question(self) -> bool:
+        result = False
+        if self.type in self.QUESTION_TYPES:
+            result = True
+        return result
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, db_comment="question FK")
-    title = models.CharField(max_length=255, db_comment="title")
-    order = models.SmallIntegerField(db_comment="order")
+    component = models.ForeignKey(Component, related_name="+", on_delete=models.CASCADE, db_comment="question FK")
+    text = models.CharField(verbose_name=_("text"), max_length=255, db_comment="title")
+    order = models.SmallIntegerField(verbose_name=_("order"), default=0, db_comment="order")
+    updated_by = models.ForeignKey(
+        User,
+        verbose_name=_("updated by"),
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        db_comment="last user who updated",
+    )
+    created_at = models.DateTimeField(verbose_name=_("created at"), auto_now_add=True, db_comment="created at")
+    update_at = models.DateTimeField(verbose_name=_("updated at"), auto_now=True, db_comment="updated at")
 
     class Meta:
         db_table = "choice"
+        verbose_name = _("choice")
+        verbose_name_plural = _("choices")
+
+
+class Submit(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, db_comment="form FK")
+    form_title = models.CharField(verbose_name=_("form title"), max_length=255, db_comment="title")
+    user = models.ForeignKey(
+        User, verbose_name=_("user"), on_delete=models.SET_NULL, null=True, db_comment="user who submitted"
+    )
+    created_at = models.DateTimeField(verbose_name=_("created at"), auto_now_add=True, db_comment="created at")
+
+    class Meta:
+        db_table = "submit"
+        verbose_name = _("submit")
+        verbose_name_plural = _("submits")
+
+
+class Answer(models.Model):
+    submit = models.ForeignKey(
+        Submit, verbose_name=_("submit"), related_name="+", on_delete=models.CASCADE, db_comment="submit FK"
+    )
+    component = models.ForeignKey(
+        Component, verbose_name=_("component"), related_name="+", on_delete=models.DO_NOTHING, db_comment="component FK"
+    )
+    question_title = models.CharField(
+        verbose_name=_("question title"), max_length=255, blank=True, db_comment="component title"
+    )
+    answer = models.CharField(verbose_name=_("answer"), max_length=1000, db_comment="text")
+    choice = models.ForeignKey(
+        Choice,
+        verbose_name=_("choice"),
+        related_name="+",
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+        db_comment="choice FK",
+    )
+    choice_text = models.CharField(verbose_name=_("choice text"), max_length=255, blank=True, db_comment="choice text")
+    created_at = models.DateTimeField(verbose_name=_("created at"), auto_now_add=True, db_comment="created at")
+
+    class Meta:
+        db_table = "answer"
+        verbose_name = _("answer")
+        verbose_name_plural = _("answers")
