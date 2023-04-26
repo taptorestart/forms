a846ca1ac539f3aac4d21e427a193ad54ebbb647
taptorestart <taptorestart@gmail.com>
2023-04-26 16:56:01 +0900

    ENH: Update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS_MODULE"))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
