d9dbf4d9e5bb5e14180b22dd5da6edb82ac95d80
taptorestart <taptorestart@gmail.com>
2023-12-30 18:57:23 +0900

    ENH: Apply the unfold theme to admin

--- backend/apps/forms/admin.py
+++ backend/apps/forms/admin.py
@@ -1,26 +1,44 @@
 from celery.result import AsyncResult
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group, User
 from django.http import Http404, JsonResponse, FileResponse
 from django.urls import path
 from django.utils.safestring import mark_safe
 from rest_framework import status
+from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from apps.forms.models import Form, Component, Choice, Submit
 from apps.forms.tasks import download_xlsx
 
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+
+@admin.register(Group)
+class GroupAdmin(GroupAdmin, ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+    list_display = ("id", "username", "email", "is_superuser", "is_staff", "is_active")
+    list_display_links = ("id", "username")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("id", "username", "email")
+    filter_horizontal = ("groups", "user_permissions")
+
 
 @admin.register(Form)
-class FormAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "slug",
-        "title",
-        "start_date",
-        "end_date",
-        "updated_by",
-        "created_at",
-        "update_at",
-    )
+class FormAdmin(ModelAdmin):
+    list_display = ("id", "slug", "title", "start_date", "end_date", "updated_by", "created_at", "update_at")
     readonly_fields = ("updated_by",)
 
     def save_model(self, request, obj: Form, form, change):
@@ -29,7 +47,7 @@ class FormAdmin(admin.ModelAdmin):
 
 
 @admin.register(Component)
-class ComponentAdmin(admin.ModelAdmin):
+class ComponentAdmin(ModelAdmin):
     list_display = (
         "id",
         "form_slug",
@@ -44,10 +62,7 @@ class ComponentAdmin(admin.ModelAdmin):
         "created_at",
         "update_at",
     )
-    readonly_fields = (
-        "updated_by",
-        "is_question",
-    )
+    readonly_fields = ("updated_by", "is_question")
     raw_id_fields = ("form",)
 
     def get_queryset(self, request):
@@ -70,16 +85,8 @@ class ComponentAdmin(admin.ModelAdmin):
 
 
 @admin.register(Choice)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "component_title",
-        "text",
-        "order",
-        "updated_by",
-        "created_at",
-        "update_at",
-    )
+class ChoiceAdmin(ModelAdmin):
+    list_display = ("id", "component_title", "text", "order", "updated_by", "created_at", "update_at")
     readonly_fields = ("updated_by",)
     raw_id_fields = ("component",)
 
@@ -100,14 +107,8 @@ class ChoiceAdmin(admin.ModelAdmin):
 
 
 @admin.register(Submit)
-class SubmitAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "form_slug",
-        "form_title",
-        "user",
-        "answer",
-    )
+class SubmitAdmin(ModelAdmin):
+    list_display = ("id", "form_slug", "form_title", "user", "answer")
     list_filter = ("form__slug",)
 
     change_list_template = "list.html"
