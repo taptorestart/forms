8a18ca7337ab5dbbbf3d599890865f62ce0ce772
Tap to restart! <taptorestart@gmail.com>
2023-12-31 16:47:04 +0900

    ENH: Change DB from mysql to postgres

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
